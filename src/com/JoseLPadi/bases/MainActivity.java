58f155ab62bce87edd8dba6952ff8a09123aa32b
jose <jose@joselala.(none)>
2014-08-25 17:16:18 -0300

    implementacion base de tabs

--- src/com/JoseLPadi/bases/MainActivity.java
+++ src/com/JoseLPadi/bases/MainActivity.java
@@ -1,15 +1,41 @@
 package com.JoseLPadi.bases;
 
-import android.app.Activity;
+
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
+import android.widget.TabHost;
+public class MainActivity extends FragmentActivity {
+	
+	//My tabhost
+    private TabHost tabHost;
+  
 
-public class MainActivity extends Activity {
-
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Resources res = getResources();
+		tabHost = (TabHost)findViewById(android.R.id.tabhost);
+		
+        tabHost.setup();
+        
+        TabHost.TabSpec spec=tabHost.newTabSpec("mitab1");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("",res.getDrawable(android.R.drawable.ic_btn_speak_now));
+        tabHost.addTab(spec);
+         
+        spec=tabHost.newTabSpec("mitab2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("",res.getDrawable(android.R.drawable.ic_btn_speak_now));
+        tabHost.addTab(spec);
+         
+        tabHost.setCurrentTab(0);
+      //  initializeTabs();
+
+        
 	}
+		
 }
